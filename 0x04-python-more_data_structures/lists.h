90b77f615f0b6bff87d76db67440a858fc35903b
veehra <mmesoma.vera@gmail.com>
2023-01-11 14:37:18 +0100

    update

--- 0x04-python-more_data_structures/lists.h
+++ 0x04-python-more_data_structures/lists.h
@@ -5,3 +5,4 @@ def common_elements(set_1, set_2):
 def only_diff_elements(set_1, set_2):
 def number_keys(a_dictionary):
 def print_sorted_dictionary(a_dictionary):
+def update_dictionary(a_dictionary, key, value):
